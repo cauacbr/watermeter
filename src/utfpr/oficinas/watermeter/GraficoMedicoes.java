d642a6fa6e3428f6c35f57f7d79af7766087021c
Rafael <Rafael@H54>
2014-07-02 18:34:15 -0300

    Detectando em qual barra do gráfico foi clicado. Próximo: Expandir para meses

--- src/utfpr/oficinas/watermeter/GraficoMedicoes.java
+++ src/utfpr/oficinas/watermeter/GraficoMedicoes.java
@@ -17,6 +17,12 @@ import android.view.View;
 
 public class GraficoMedicoes extends View{
 	
+	private static final int ZOOM_YEAR = 0;
+	private static final int ZOOM_MONTH = 1;
+	private static final int ZOOM_WEEK = 2;
+	private static final int ZOOM_DAT = 3;
+	private Integer currentRefZoom = null;
+	
 	private Paint background, barsPaint;
 	private int barMargin, barWidth, barHeight;
 	private Paint bars[];
@@ -73,10 +79,10 @@ public class GraficoMedicoes extends View{
 	                
 	                if(-(endX - startX) > viewWidth/4) { //transladar para esquerda
 	                	
-	                	Log.d("motion", "Transladar Esquerda...");
-	                	Log.d("motion", "MIN_TRANSLATION: " + MIN_TRANSLATION);
-	                	Log.d("motion", "Atual: " + actualTranslation );
-	                	Log.d("motion", "Pretendido: " + (actualTranslation - 2*(barWidth + barMargin)));
+//	                	Log.d("motion", "Transladar Esquerda...");
+//	                	Log.d("motion", "MIN_TRANSLATION: " + MIN_TRANSLATION);
+//	                	Log.d("motion", "Atual: " + actualTranslation );
+//	                	Log.d("motion", "Pretendido: " + (actualTranslation - 2*(barWidth + barMargin)));
 	                		                	
                 		int aux;
 	                	
@@ -91,10 +97,10 @@ public class GraficoMedicoes extends View{
 	                	
 	                }else if((endX - startX) > viewWidth/4) { //transladar para direita
 	                	
-	                	Log.d("motion", "Transladar Direita...");
-	                	Log.d("motion", "MAX_TRANSLATION: " + MAX_TRANSLATION);
-	                	Log.d("motion", "Atual: " + actualTranslation );
-	                	Log.d("motion", "Pretendido: " + (actualTranslation + 2*(barWidth + barMargin)));
+//	                	Log.d("motion", "Transladar Direita...");
+//	                	Log.d("motion", "MAX_TRANSLATION: " + MAX_TRANSLATION);
+//	                	Log.d("motion", "Atual: " + actualTranslation );
+//	                	Log.d("motion", "Pretendido: " + (actualTranslation + 2*(barWidth + barMargin)));
 	                		                	
                 		int aux;
 	                	
@@ -110,9 +116,17 @@ public class GraficoMedicoes extends View{
 	                }
 	                
 	                if(Math.abs(startX - endX) < 50) { //Deslizou no máximo 50 pixels
-	                    //Foi um click, verificar qual barra MUDAR ISSO PARA UMA LóGICA XY (Xstart - Xend for pequeno)
 	                	
 	                	Log.d("motion", "clique na view do gráfico");
+	                	
+	                	for(Rect barraAtual: barras) {
+	                		
+	                		if(barraAtual.contains(startX, startY)) {
+	                			Log.d("motion", "Clique na Barra " + barras.indexOf(barraAtual));
+	                		}
+	                		
+	                	}
+	                	
 	                }
 	            }
 	        }
@@ -129,105 +143,122 @@ public class GraficoMedicoes extends View{
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		
-		int viewWidth = getWidth();
-		int viewHeight = getHeight();
+		plotarGrafico(canvas, ZOOM_YEAR, -1);
 		
-		path.addRect(viewWidth/20, 0, (float) (viewWidth*0.95), viewHeight, Direction.CW);
-		canvas.clipPath(path);
-	    
-
-		int sumConsumoLitros = 0;
-		int sumConsumoMililitros = 0;
-
-		background = new Paint(Paint.ANTI_ALIAS_FLAG);
-		background.setColor(Color.WHITE);
-		background.setStrokeWidth(2);
+	}
+	
+	private void plotarGrafico(Canvas canvas, int zoom, int referenceZoom) {
 		
-		barsPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		barsPaint.setColor(Color.WHITE);
-		
-		barWidth = (int) (viewWidth/7.7);
-		barMargin = (int) (barWidth/6.5);
-		
-		barHeight = (int) (viewHeight*0.65);
-		
-		//Desenhando as linhas de Fundo
-		
-		canvas.drawLine(viewWidth/20, (float) (viewHeight*0.75), (float) (viewWidth*0.95), (float) (viewHeight*0.75), background);
-		canvas.drawLine(viewWidth/20, viewHeight/20, viewWidth/20, (float) (viewHeight*0.75), background);
+		switch(zoom) {
+			
+			case ZOOM_YEAR: /********************************************************** ZOOM_YEAR **********************************************************************************/
 				
-		// Desenhando as barras
-		
-		int maiorMedicaoAcumulada = 0;
-		
-		for(int i = dbManager.getMinYear() ; i <= dbManager.getMaxYear(); i++) {
-			
-			for(Medicao medicao: dbManager.getMedicoesByYear(i)) {
-				sumConsumoLitros += medicao.getLitro();
-				sumConsumoMililitros += medicao.getMiliLitro();
-			}
-			
-			sumConsumoLitros += sumConsumoMililitros/1000;
-			sumConsumoMililitros = sumConsumoMililitros%1000;
-			
-			quantities.add(sumConsumoLitros + sumConsumoMililitros/500); //arredonda para cima o comprimento da barra
-			
-			quantitiesLabel.add(String.valueOf(sumConsumoLitros) + "," + String.valueOf(sumConsumoMililitros/100 + " L"));
-			datesLabel.add(String.valueOf(i));
-			
-			if((sumConsumoLitros + sumConsumoMililitros%500) > maiorMedicaoAcumulada)
-				maiorMedicaoAcumulada = (sumConsumoLitros + sumConsumoMililitros%500);
-			
-			sumConsumoLitros = sumConsumoMililitros = 0;
-			
-		}
-		
-		//*********************************
-		
-		MIN_TRANSLATION = -1*((dbManager.getMaxYear() - dbManager.getMinYear()) - 5)*(barWidth + barMargin); //total de barras - 5. 6 é o número que cabe na tela
-		
-		Log.d("motion", "max_year: " + dbManager.getMaxYear());
-		
-		//*********************************
-		
-		int offset = barMargin;
-		float x0 = viewWidth/20;
-		float y0 = (float) (viewHeight*0.75);
-		
-		Paint teste = new Paint(Paint.ANTI_ALIAS_FLAG);
-		teste.setTextSize((float)(barWidth/3));
-		teste.setColor(Color.WHITE);
-		teste.setStyle(Style.FILL);		
-		
-		
-		for(Integer bar : quantities) {
-			
-			auxRect.set((int) (x0 + offset + actualTranslation), (int) (y0 - (barHeight * ((float)(bar)/maiorMedicaoAcumulada))), (int) (x0 + offset + actualTranslation + barWidth), (int) (y0));
-			
-			barras.add(auxRect);
-			canvas.drawRect(auxRect, barsPaint);
-			
-			canvas.save();
-			canvas.rotate(-60,  x0 + offset + actualTranslation + (barWidth - teste.measureText(datesLabel.get(quantities.indexOf(bar)))), (float) ((y0 - (barHeight * ((float)(bar)/maiorMedicaoAcumulada))) - 0.3*teste.getTextSize()));
-			
+				int viewWidth = getWidth();
+				int viewHeight = getHeight();
+				
+				path.addRect(viewWidth/20, 0, (float) (viewWidth*0.95), viewHeight, Direction.CW);
+				canvas.clipPath(path);
+			    
 
-			// Desenhando as legendas de consumo
-			
-			canvas.drawText(quantitiesLabel.get(quantities.indexOf(bar)), x0 + offset + actualTranslation + (barWidth - teste.measureText(datesLabel.get(quantities.indexOf(bar)))), (float) ((y0 - (barHeight * ((float)(bar)/maiorMedicaoAcumulada))) - 0.3*teste.getTextSize()), teste);
-			//canvas.drawText(String.valueOf((float)(bar)/maiorMedicaoAcumulada), x0 + offset + (barWidth - teste.measureText(datesLabel.get(quantities.indexOf(bar)))), (float) ((y0 - (barHeight * ((float)(bar)/maiorMedicaoAcumulada))) - 0.3*teste.getTextSize()), teste);
-			
-			canvas.restore();
-			
-			// Desenhando as legendas de datas
-			
-			canvas.drawText(datesLabel.get(quantities.indexOf(bar)), x0 + offset + actualTranslation + ((barWidth - teste.measureText(datesLabel.get(quantities.indexOf(bar)))) /2 ), y0 + teste.getTextSize() + 5, teste);
-			
-			offset += barMargin + barWidth;
-			
+				int sumConsumoLitros = 0;
+				int sumConsumoMililitros = 0;
+
+				background = new Paint(Paint.ANTI_ALIAS_FLAG);
+				background.setColor(Color.WHITE);
+				background.setStrokeWidth(2);
+				
+				barsPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+				barsPaint.setColor(Color.WHITE);
+				
+				barWidth = (int) (viewWidth/7.7);
+				barMargin = (int) (barWidth/6.5);
+				
+				barHeight = (int) (viewHeight*0.65);
+				
+				//Desenhando as linhas de Fundo
+				
+				canvas.drawLine(viewWidth/20, (float) (viewHeight*0.75), (float) (viewWidth*0.95), (float) (viewHeight*0.75), background);
+				canvas.drawLine(viewWidth/20, viewHeight/20, viewWidth/20, (float) (viewHeight*0.75), background);
+						
+				// Desenhando as barras
+				
+				int maiorMedicaoAcumulada = 0;
+				
+				for(int i = dbManager.getMinYear() ; i <= dbManager.getMaxYear(); i++) {
+					
+					for(Medicao medicao: dbManager.getMedicoesByYear(i)) {
+						sumConsumoLitros += medicao.getLitro();
+						sumConsumoMililitros += medicao.getMiliLitro();
+					}
+					
+					sumConsumoLitros += sumConsumoMililitros/1000;
+					sumConsumoMililitros = sumConsumoMililitros%1000;
+					
+					quantities.add(sumConsumoLitros + sumConsumoMililitros/500); //arredonda para cima o comprimento da barra
+					
+					quantitiesLabel.add(String.valueOf(sumConsumoLitros) + "," + String.valueOf(sumConsumoMililitros/100 + " L"));
+					datesLabel.add(String.valueOf(i));
+					
+					if((sumConsumoLitros + sumConsumoMililitros%500) > maiorMedicaoAcumulada)
+						maiorMedicaoAcumulada = (sumConsumoLitros + sumConsumoMililitros%500);
+					
+					sumConsumoLitros = sumConsumoMililitros = 0;
+					
+				}
+				
+				//*********************************
+				
+				MIN_TRANSLATION = -1*((dbManager.getMaxYear() - dbManager.getMinYear()) - 5)*(barWidth + barMargin); //total de barras - 5. 6 é o número que cabe na tela
+				
+				//*********************************
+				
+				int offset = barMargin;
+				float x0 = viewWidth/20;
+				float y0 = (float) (viewHeight*0.75);
+				
+				Paint teste = new Paint(Paint.ANTI_ALIAS_FLAG);
+				teste.setTextSize((float)(barWidth/3));
+				teste.setColor(Color.WHITE);
+				teste.setStyle(Style.FILL);		
+				
+				barras.clear(); //Limpa lista de barras atuais
+				
+				for(Integer bar : quantities) {
+					
+					auxRect = new Rect();
+					auxRect.set((int) (x0 + offset + actualTranslation), (int) (y0 - (barHeight * ((float)(bar)/maiorMedicaoAcumulada))), (int) (x0 + offset + actualTranslation + barWidth), (int) (y0));
+					
+					barras.add(auxRect);
+					canvas.drawRect(auxRect, barsPaint);
+					
+					canvas.save();
+					canvas.rotate(-60,  x0 + offset + actualTranslation + (barWidth - teste.measureText(datesLabel.get(quantities.indexOf(bar)))), (float) ((y0 - (barHeight * ((float)(bar)/maiorMedicaoAcumulada))) - 0.3*teste.getTextSize()));
+					
+
+					// Desenhando as legendas de consumo
+					
+					canvas.drawText(quantitiesLabel.get(quantities.indexOf(bar)), x0 + offset + actualTranslation + (barWidth - teste.measureText(datesLabel.get(quantities.indexOf(bar)))), (float) ((y0 - (barHeight * ((float)(bar)/maiorMedicaoAcumulada))) - 0.3*teste.getTextSize()), teste);
+					//canvas.drawText(String.valueOf((float)(bar)/maiorMedicaoAcumulada), x0 + offset + (barWidth - teste.measureText(datesLabel.get(quantities.indexOf(bar)))), (float) ((y0 - (barHeight * ((float)(bar)/maiorMedicaoAcumulada))) - 0.3*teste.getTextSize()), teste);
+					
+					canvas.restore();
+					
+					// Desenhando as legendas de datas
+					
+					canvas.drawText(datesLabel.get(quantities.indexOf(bar)), x0 + offset + actualTranslation + ((barWidth - teste.measureText(datesLabel.get(quantities.indexOf(bar)))) /2 ), y0 + teste.getTextSize() + 5, teste);
+					
+					offset += barMargin + barWidth;
+					
+				}
+
+				this.setOnTouchListener(graficoDragListener);
+				
+				break;
+				
+				/************************************************************* FIM ZOOM_YEAR **************************************************************************************/
+				
+		
 		}
 
-		this.setOnTouchListener(graficoDragListener);
-		
 	}
 	
 }
